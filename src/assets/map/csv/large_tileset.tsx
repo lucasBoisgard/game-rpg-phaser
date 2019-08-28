1047a9588fa495f080de0e5363cc585b83ce32d7
lucas <lucas.boisgard@epitech.eu>
2019-08-28 23:01:00 +0200

    first map + debbug collider

--- src/assets/map/csv/large_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="large_tileset" tilewidth="32" tileheight="32" tilecount="2016" columns="32">
- <image source="../tiles/large_tileset.png" width="1024" height="2016"/>
-</tileset>
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 var config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
-    width: 960,
-    height: 960,
+    width: 900,
+    height: 600,
     scene: {
         preload: preload,
         create: create,
@@ -10,36 +10,101 @@ var config = {
     }
 };
 
+let cursors;
+let player;
+let showDebug = false;
+let solid;
 
 var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('tiles', 'assets/map/tiles/tmw_desert_spacing.png');
-    this.load.tilemapCSV("mapCsv", 'assets/map/csv/map2_terrain.csv')
-    this.load.tilemapTiledJSON('map', 'assets/map/maps/map1.json');
+    this.load.image('tiles', 'assets/map/tiles/terrain_base.png');
+    this.load.tilemapTiledJSON('map', 'assets/map/csv/map2.json');
+
+    this.load.atlas("atlas", "assets/character/naked.png", "assets/character/naked.json");
+
+    // this.load.tilemapCSV("terrain00", 'assets/map/csv/map2_terrain00.csv')
+    // this.load.tilemapCSV("terrain00", 'assets/map/csv/map2_terrain_solid.csv')
 }
 
 function create ()
 {
-    // map = this.make.tilemap({ key: 'mapCsv' });
 
-    // var tiles = map.addTilesetImage('Desert', 'tiles');
+    const map = this.make.tilemap({ key: "map" });
 
-    // layer = map.createDynamicLayer('Ground', tiles, 0, 0).setVisible(false);
+    const tileset = map.addTilesetImage("terrain", "tiles");
+    const layer0 = map.createStaticLayer("terrain00", tileset, 0, 0);
+    solid = map.createStaticLayer("terrain_solid", tileset, 0, 0);
 
-    // rt = this.add.renderTexture(0, 0, 800, 600);
-    // console.log(rt)
-    // When loading a CSV map, make sure to specify the tileWidth and tileHeight!
-    const map = this.make.tilemap({ key: "mapCsv", tileWidth: 32, tileHeight: 32 });
-    const tileset = map.addTilesetImage("tiles");
-    const tileset = map.addTilesetImage("tiles2");
-    const layer = map.createStaticLayer(0, tileset, 0, 0); // layer index, tileset, x, y
+
+    solid.setCollisionByProperty({ solid: true });
+
+    const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point");
+
+    // player = this.physics.add
+    //     .sprite(spawnPoint.x, spawnPoint.y, "atlas", "misa-front")
+    //     .setSize(30, 40)
+    //     .setOffset(0, 24);
+
+    //     this.physics.add.collider(player, worldLayer);
+
+
+
+
+
+
+
+  const camera = this.cameras.main;
+  camera.zoom = 2;
+  // Set up the arrows to control the camera
+  const cursors = this.input.keyboard.createCursorKeys();
+  controls = new Phaser.Cameras.Controls.FixedKeyControl({
+    camera: camera,
+    left: cursors.left,
+    right: cursors.right,
+    up: cursors.up,
+    down: cursors.down,
+    speed: 0.5
+  });
+
+  // Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
+  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+
+    debugGraphics = this.add.graphics();
+    map.renderDebug(debugGraphics, {
+        tileColor: null,
+        collidingTileColor: new Phaser.Display.Color(243, 134, 48, 128), 
+        faceColor: new Phaser.Display.Color(40, 39, 37, 255) 
+      });
+
+      this.input.keyboard.on('keydown_C', function (event)
+      {
+          showDebug = !showDebug;
+          drawDebug();
+      });
+  
+
 }
 
-function update ()
-{
-    // rt.clear();
+function update(time, delta) {
+    // Apply the controls to the camera each update tick of the game
+    controls.update(delta);
+}
 
-    // rt.draw(layer);
+
+function drawDebug ()
+{
+    debugGraphics.clear();
+    if (showDebug)
+    {
+        // Pass in null for any of the style options to disable drawing that component
+        solid.renderDebug(debugGraphics, {
+            tileColor: null, // Non-colliding tiles
+            collidingTileColor: new Phaser.Display.Color(243, 134, 48, 200), // Colliding tiles
+            faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Colliding face edges
+        });
+    }
+
 }
