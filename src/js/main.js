55f373c70cd7384f3ffb51bee0ad20b9cd7fe7c4
lucas <lucas.boisgard@epitech.eu>
2019-08-29 00:07:20 +0200

    start character animation

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,12 @@ var config = {
     parent: 'phaser-example',
     width: 900,
     height: 600,
+    physics: {
+        default: "arcade",
+        arcade: {
+          gravity: { y: 0 }
+        }
+      },
     scene: {
         preload: preload,
         create: create,
@@ -22,7 +28,7 @@ function preload ()
     this.load.image('tiles', 'assets/map/tiles/terrain_base.png');
     this.load.tilemapTiledJSON('map', 'assets/map/csv/map2.json');
 
-    this.load.atlas("atlas", "assets/character/naked.png", "assets/character/naked.json");
+    this.load.atlas("atlas", "assets/character/naked.png", "assets/character/sprites.json");
 
     // this.load.tilemapCSV("terrain00", 'assets/map/csv/map2_terrain00.csv')
     // this.load.tilemapCSV("terrain00", 'assets/map/csv/map2_terrain_solid.csv')
@@ -40,20 +46,66 @@ function create ()
 
     solid.setCollisionByProperty({ solid: true });
 
-    const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point");
+    const spawnPoint = map.findObject("Objects", obj => obj.name === "spawn");
 
-    // player = this.physics.add
-    //     .sprite(spawnPoint.x, spawnPoint.y, "atlas", "misa-front")
-    //     .setSize(30, 40)
-    //     .setOffset(0, 24);
-
+    player = this.physics.add
+        .sprite(spawnPoint.x, spawnPoint.y, "atlas", "face")
+        .setSize(30, 40)
+        .setOffset(0, 24);
+    console.log(player)
     //     this.physics.add.collider(player, worldLayer);
 
 
+    ////////////////////////////////////////////////////////
 
 
+    const anims = this.anims;
+    anims.create({
+      key: "face",
+      frames: anims.generateFrameNames("atlas", {
+        prefix: "face.",
+        start: 0,
+        end: 3,
+        zeroPad: 3
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+    anims.create({
+      key: "misa-right-walk",
+      frames: anims.generateFrameNames("atlas", {
+        prefix: "misa-right-walk.",
+        start: 0,
+        end: 3,
+        zeroPad: 3
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+    anims.create({
+      key: "misa-front-walk",
+      frames: anims.generateFrameNames("atlas", {
+        prefix: "misa-front-walk.",
+        start: 0,
+        end: 3,
+        zeroPad: 3
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+    anims.create({
+      key: "misa-back-walk",
+      frames: anims.generateFrameNames("atlas", {
+        prefix: "misa-back-walk.",
+        start: 0,
+        end: 3,
+        zeroPad: 3
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
 
-
+    ////////////////////////////////////////////////////////
 
   const camera = this.cameras.main;
   camera.zoom = 2;
@@ -69,6 +121,7 @@ function create ()
   });
 
   // Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
+  camera.startFollow(player);
   camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
 
